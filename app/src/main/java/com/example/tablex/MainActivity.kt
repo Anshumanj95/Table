fa2393f4f1bd52f3db2a413b2f482426852e877b
Anshuman Jain <Anshumanj95@gmail.com>
2021-06-20 22:19:58 +0530

    first commit

---
+++ app/src/main/java/com/example/tablex/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.tablex
+
+import android.R
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.SeekBar
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tablex.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.seekbaar.max=20
+        binding.seekbaar.min=1
+        val table=ArrayList<String>()
+        for (i in 1..10){
+            table.add("1 X $i = ${i*1}")
+        }
+        binding.text.text="Table of 1"
+        val adapter=ArrayAdapter(this, R.layout.simple_list_item_1,table)
+        binding.table.adapter=adapter
+        binding.seekbaar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if(fromUser){
+                    fetchTable(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+    }
+    @SuppressLint("SetTextI18n")
+    private fun fetchTable(number:Int){
+        val table=ArrayList<String>()
+        for (i in 1..10){
+            table.add("$number X $i = ${i*number}")
+        }
+        binding.text.text="Table of $number"
+       val adapter=ArrayAdapter(this, R.layout.simple_list_item_1,table)
+        binding.table.adapter=adapter
+    }
+}
